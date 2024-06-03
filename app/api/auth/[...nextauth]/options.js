79870a15518d9cdc4a2e04db2bd968aaa56aeb9f
heliosalesjr <heliosalesjr@gmail.com>
2024-06-03 19:26:54 -0300

    api development

---
+++ app/api/auth/[...nextauth]/options.js
@@ -0,0 +1,14 @@
+import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+
+export const options = {
+    providers: [
+        GitHubProvider({
+            profile(profile) {
+                console.log('profile')
+            }
+        }
+        )
+    ];
+
+};
