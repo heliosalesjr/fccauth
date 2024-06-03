58737358918392b60f5ac568f0f18f2d7e99365f
heliosalesjr <heliosalesjr@gmail.com>
2024-06-03 20:06:14 -0300

    login!

--- app/api/auth/[...nextauth]/options.js
+++ app/api/auth/[...nextauth]/options.js
@@ -5,10 +5,48 @@ export const options = {
     providers: [
         GitHubProvider({
             profile(profile) {
-                console.log('profile')
-            }
+                console.log("Profile GitHub: ", profile);
+        
+                let userRole = "GitHub User";
+                if (profile?.email == "jake@claritycoders.com") {
+                  userRole = "admin";
+                }
+        
+                return {
+                  ...profile,
+                  role: userRole,
+                };
+              },
+              clientId: process.env.GITHUB_ID,
+              clientSecret: process.env.GITHUB_Secret,
+            }),
+
+        GoogleProvider({
+            profile(profile) {
+                console.log('profile google: ', profile)
+
+                return {
+                    ...profile,
+                    id: profile.sub,
+                    role: userRole,
+                };
+            },
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_Secret,
         }
-        )
-    ];
+        ),
+    ],
+
+    callbacks: {
+        async jwt({token, user}) {
+            if (user) token.role = user.role
+            return token
+        },
+
+        async session ({session, token}) {
+            if (session?.user) session.user.role = token.role
+            return session
+        }
+    }
 
 };
